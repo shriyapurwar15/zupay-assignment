cd923a8d4d3a7100d09b08478e1426f9766557e6
DJ <98403273+shriyapurwar15@users.noreply.github.com>
2024-08-24 23:54:41 +0530

    added cors origin in backend

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,22 @@ const postRoutes = require("./routes/postRoutes")
 
 
 const app = express();
-app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use(express.json());
+var allowlist = ['http://localhost:3000', 'http://localhost:5173', 'https://zupay-assignment-1.onrender.com']
+var corsOptionsDelegate = function (req, callback) {
+    var corsOptions;
+    if (allowlist.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    } else {
+        corsOptions = { origin: false } // disable CORS for this request
+    }
+    callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+app.use(require('cors')(corsOptionsDelegate));
+app.use(cors())
 
 connectDB();
 
